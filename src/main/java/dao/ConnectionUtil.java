83dbd190d47430de5405c5a021fb9164c3cdd06c
karthik p <karthik.pannerselvam@fssa.freshworks.com>
2023-08-09 09:45:17 +0530

    completed the taskStatus

---
+++ src/main/java/dao/ConnectionUtil.java
@@ -0,0 +1,5 @@
+package dao;
+
+public class ConnectionUtil {
+	public class
+}
--- src/main/java/validation/TaskValidation.java
+++ src/main/java/validation/TaskValidation.java
@@ -2,29 +2,42 @@ package validation;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import validation.exception.InvalidTaskException;
 
 public class TaskValidation {
-	public  boolean TaskName(String taskName ) throws InvalidTaskException{
+	public boolean taskName(String taskName) throws InvalidTaskException {
 		boolean match = false;
-		
-		if(taskName ==null) {
+
+		if (taskName == null) {
 			return false;
 		}
-		
-		String regex = "^[A-Za-z]\\w{2,29}$";
+
+		String regex = "^[A-Za-z]\\w{2,200}$";
 		Pattern tName = Pattern.compile(regex);
 		Matcher m = tName.matcher(regex);
 		match = m.matches();
-		
-		if(match) {
+
+		if (match) {
 			System.out.println("The Taskname is valid");
 		} else {
 			throw new InvalidTaskException("The Taskname is not valid");
 		}
 		return match;
 	}
-	
-	
-	
+
+	public boolean taskStatus(String taskStatus) throws InvalidTaskException {
+		boolean match = false;
+		if (taskStatus == null)
+			return match;
+
+		String status = "PENDING";
+		match = status.equalsIgnoreCase(status);
+		if (match) {
+			System.out.println("The task is PENDING");
+		} else {
+			System.out.println("The task is COMPLETED");
+		}
+		return match;
+	}
 }
